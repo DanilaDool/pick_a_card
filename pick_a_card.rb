c36a3ebcbc985d7db069c8debbeff88b019e0a07
DanilaDool <Levhenko-2626@mail.ru>
2023-06-22 23:45:31 +0300

    add suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 #BlaBla
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+suits = File.readlines('/home/danila/ruby/pick_a_card/pick_a_card/data/suits.txt', chomp: true)
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
