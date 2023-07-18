350a0494ea93971389623e3ca8fccab166afae0d
DanilaDool <Levhenko-2626@mail.ru>
2023-07-18 19:51:26 +0300

    add greetings

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,6 @@
 #BlaBla
+puts "Это программа выводит тебе столько карт сколько захочешь!"
+
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 suits = File.readlines('/home/danila/ruby/pick_a_card/pick_a_card/data/suits.txt', chomp: true)
